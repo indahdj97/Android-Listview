68d26c167bb12f5ed280eb0a4ddb366c38e9bf1f
indahdj97 <indahdj4@gmail.com>
2019-06-21 15:27:30 +0700

    first commit

---
+++ app/src/main/java/com/example/indah/mengenalbuah/Konstanta.java
@@ -0,0 +1,6 @@
+package com.example.indah.mengenalbuah;
+
+public class Konstanta {
+    public static final String DATANAMA = "datanama" ;
+    public static final String DATAGAMBAR = "datagambar";
+}
